9acc4e56b5c769ab23ce30e2abc0d4c9ce130482
Kohei Yasumura <yasumura@f-plat.com>
2012-06-14 07:59:13 +0900

    add remove marker

--- app.js
+++ app.js
@@ -3,38 +3,43 @@ var app = express.createServer();
 var io = require('socket.io').listen(app);
 var sha246 = require('./sha-256');
 app.set('view options',{layout:false});
-var sub_channel_counter = {};
+
 //var host = 'yas65.jit.su';
 var host = 'localhost:8888';
+
+var sub_channel_counter = {};
+
 app.get("/",function(request,response){
 	var channel = sha246.convert((new Date).toString());
-	io.of('/' + channel).on('connection',function(client){
-		var sub_channel = 0
-		if(sub_channel_counter[channel] != null){
-			sub_channel = sub_channel_counter[channel] + 1;
-		}
-		sub_channel_counter[channel] = sub_channel
-		client.set('sub_channel',sub_channel);
-		console.log('Client connected...No:' + channel + "/" + sub_channel);
-		client.on("join",function(data){
-			client.broadcast.emit('join',data);
-		});
-		client.on("update_location",function(data){
-			console.log(data);
-			client.get('sub_channel',function(err,sub_channel){
-				console.log(sub_channel)
-				client.broadcast.emit('position_update',{lat:data.lat,lng:data.lng,sub_channel:sub_channel});
-			});
-		});
-	});
-	response.render(__dirname + '/views/index.ejs',{channel: channel,host: host});	
+	response.redirect("/" + channel);
+	//response.render(__dirname + '/views/index.ejs',{channel: channel,host: host});	
 });
 
 app.get("/:channel",function(request,response){
 	var channel = request.params.channel;
-	console.log(request.params.channel);
 	io.of('/' + channel).on('connection',function(client){
-		console.log('Client connected...No:' + channel);
+		var sub_channel = 0;
+		client.on("join",function(){
+			if(sub_channel_counter[channel] != null){
+				sub_channel = sub_channel_counter[channel] + 1;
+			}
+			sub_channel_counter[channel] = sub_channel;
+			client.set('sub_channel',sub_channel);
+			console.log('Client connected...No:' + channel + "/" + sub_channel);			
+		});
+		client.on("update_location",function(data){
+			console.log(data);
+			client.get('sub_channel',function(err,sub_channel){
+				console.log(sub_channel);
+				console.log(data);
+				client.broadcast.emit('position_update',{lat:data.lat,lng:data.lng,sub_channel:sub_channel});
+			});
+		});
+		client.on("disconnect",function(){
+			client.get('sub_channel',function(err,sub_channel){
+				client.broadcast.emit('remove_marker',{sub_channel:sub_channel});
+			});
+		});
 	});
 	response.render(__dirname + '/views/index.ejs',{channel: channel,host: host});
 });
